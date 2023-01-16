7073387dc7143e93691950f2c0869db7f180bfd6
Jess Penners <jpenners@email.arizona.edu>
2023-01-16 07:46:24 -0800

    Models for: Hike, Hiker, Location, Trail

---
+++ server/src/main/java/learn/hiking/models/Hiker.java
@@ -0,0 +1,53 @@
+package learn.hiking.models;
+
+public class Hiker {
+        private int hikerId;
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String email;
+
+        public Hiker() {
+        }
+
+        public int getHikerId() {
+            return hikerId;
+        }
+
+        public void setHikerId(int hikerId) {
+            this.hikerId = hikerId;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+}
