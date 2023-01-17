14dbddd47a2a39901666dc9d0a074063a48d58f6
Jess Penners <jpenners@email.arizona.edu>
2023-01-17 09:14:12 -0800

    Hiker findById method

--- server/src/main/java/learn/hiking/data/HikerJdbcTemplateRepository.java
+++ server/src/main/java/learn/hiking/data/HikerJdbcTemplateRepository.java
@@ -1,6 +1,8 @@
 package learn.hiking.data;
 
 import learn.hiking.data.mappers.HikeMapper;
+import learn.hiking.data.mappers.HikerMapper;
+import learn.hiking.models.Hike;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import learn.hiking.models.Hiker;
@@ -17,10 +19,19 @@ public class HikerJdbcTemplateRepository implements HikerRepository {
 
     @Override
     public List<Hiker> findAll() {
+        final String sql = "select hiker_id, first_name, last_name, age, email"
+                + "from hiker limit 1000;";
+        return jdbcTemplate.query(sql, new HikerMapper());
     }
 
     @Override
-    public Hiker findById() {
+    public Hiker findById(int hikerId) {
+        final String sql = "select hiker_id, first_name, last_name,age, email, from hiker where hiker_id = ?;";
+        Hiker hiker = (Hiker)this.jdbcTemplate.query("select hiker_id, first_name, last_name, age, hiker_id, email from hiker where hiker_id = ?;",
+                new HikerMapper(), new Object[]{hikerId}).stream().findFirst().orElse((Hiker) null);
+        if (hiker != null) {
+            this.add(hiker);
+        }
         return null;
     }
 
