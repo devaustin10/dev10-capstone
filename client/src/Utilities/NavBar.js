1fb1a2bc3787b59904708a3692b853169e7e441f
Jess Penners <jpenners@email.arizona.edu>
2023-01-22 18:41:53 -0800

    Navbar updates

--- client/src/Utilities/NavBar.js
+++ client/src/Utilities/NavBar.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import { LinkContainer } from 'react-router-bootstrap'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -15,26 +16,28 @@ function NavBar() {
         <>
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home"> NavBar </Navbar.Brand>
+                <Navbar.Brand href="#home"> LogOut </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            {auth.currentUser ? (
+                        <LinkContainer to="/about" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                            <Nav.Link>
+                                About
+                            </Nav.Link>
+                        </LinkContainer>
+                    ) : null}
+              {auth.currentUser ? (
+                        <Nav.Link href="/hike" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/hikes">All Hikes</NavDropdown.Item>
+                            <NavDropdown.Item href="/myhikes">My Hikes</NavDropdown.Item>
+                            <NavDropdown.Item href="/add">Add Hike</NavDropdown.Item>
+                        </Nav.Link>
+                    ) : null}
           </Nav>
         </Navbar.Collapse>
-            </Container>
+        </Container>
         </Navbar>
         </>
     );
