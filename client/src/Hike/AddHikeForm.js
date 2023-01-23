2594f1570f514dd09185b9a04cbfc7dd948a5864
Jess Penners <jpenners@email.arizona.edu>
2023-01-23 08:09:36 -0800

    Updated plan for HikeForm and CardFactory

---
+++ client/src/Hike/AddHikeForm.js
@@ -0,0 +1,24 @@
+const [selectedState, setSelectedState] = useState();
+const [filteredTrails, setFilteredTrails] = useState();
+
+
+// Need 2 Forms
+    // First Form fetches all trails and then...
+        setSelectedState(dropdownPick)
+        // Submit button will call the filter method
+        const filterTrailsByState = (dropDownStateSelection) => {
+            return trails.filter(trail => {
+                return trail.state = dropDownStateSelection;
+            })
+        }
+
+       setFilteredTrails(filterTrailsByState());
+
+    // Build Second Form using filteredTrails
+        // build out trails dropdown from this data
+
+
+let revisedHike = {
+    state: selectedState,
+    ...secondFormsData
+}
