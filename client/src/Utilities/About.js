6c60c40a7511b16c42964650eddc025a9d537b76
Jess Penners <jpenners@email.arizona.edu>
2023-01-23 21:06:56 -0800

    profile page, photos loaded, index.scss adjusted & used

--- client/src/Utilities/About.js
+++ client/src/Utilities/About.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className="profile col-lg-6 offset-lg-3 mt-5">
             <h1>About Us</h1>
-            <p>We are a company dedicated to providing top-notch products and services. Our team is made up of experts in various fields, all committed to excellence.</p>
-            <p>Contact us at contact@ourcompany.com for any inquiries.</p>
+            <p><strong>LogOut Hiking</strong> was created to encourage users to explore nature.
+                <br></br>We enabled sharing in hopes that this will allow users to connect to other hikers near them,<br></br> 
+                as well as to see others' experiences and find new trails. <br></br>
+                <br></br>We are currently building features to bookmark hike entries the user enjoys, for easy access later.</p><br></br>
+            <p>Please reach out to us at getOut@LogOut.com for any features you would like to see added,<br></br> or any errors you experience while using our app.</p>
         </div>
     );
 }
--- client/src/Utilities/Profile.js
+++ client/src/Utilities/Profile.js
@@ -1,17 +1,25 @@
-
+import Button from "react-bootstrap/Button";
+import "../index.scss";
 
 const Profile = () => {
     return (
         <div className="profile col-lg-6 offset-lg-5">
-        <img className="img-fluid mt-5" src={process.env.PUBLIC_URL + "/images/rengoku.png"} alt="Home page" />
-            <h1>John Doe</h1>
-            <p className="title">CEO & Founder, Example</p>
-            <p>Harvard University</p>
-            <p><button>Contact</button></p>
+        <img className="profilepic img-fluid mt-2" src={process.env.PUBLIC_URL + "/images/forrestgumpprofilepic.svg"} alt="Home page" />
+            <h1 className="profileName"><strong>Forrest Gump</strong></h1>
+            <p className="title mb-3"><i>Greenbow, Alabama, United States of America</i></p>
+            <p>So I ran to the end of the road.<br></br>
+            And when I got there, I thought maybe I'd run to the end of town.<br></br>
+            And when I got there, I thought maybe I'd just run across Greenbow county.<br></br>
+            And I figured, since I run this far, maybe I'd just run across the great state of Alabama.</p>
+            <Button variant="primary" className="friendRequest mt-4">Send Friend Request</Button>
+            <Button variant="secondary" className="directMessage mt-4 ms-3">Direct Message</Button>
         </div>    
     )
 };
 export default Profile;
+
+//        <Button variant="secondary" className="mt-3 ms-2 mb-5" type="button" onClick={() => navigate("/hikes")}>Cancel</Button>
+
 
 //     <div>
 //     <img className="img-fluid mt-5" src={process.env.PUBLIC_URL + "/images/rengoku.png"} alt="Home page" />
